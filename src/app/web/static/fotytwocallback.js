0465b2cdfbd966a28cc93363a3502d8ab4b647db
Jorge Bortolotti Suarez <jbortolo@car9s4.42barcelona.com>
2024-12-03 18:21:38 +0100

    creado el front para poder conectar a la api

--- src/app/web/static/fotytwocallback.js
+++ src/app/web/static/fotytwocallback.js
@@ -1,8 +1,6 @@
 const urlparams = new URLSearchParams(window.location.search);
 let code = urlparams.get('code');
 let state = urlparams.get('state');
-console.log('Code :',code);
-console.log('State: ' ,state);
 if (code && state){
     const queryParams = new URLSearchParams({code , state}).toString();
     const url = `http://localhost:8000/login/handleCallback?${queryParams}`;
@@ -14,13 +12,17 @@ if (code && state){
     })
     .then(response => response.json())
     .then(data => {
-        console.log('Data:', data);
         if (data.access && data.refresh_token){
             document.cookie = `access_token=${data.access_token}`;
             document.cookie = `refresh_token=${data.refresh_token}`;
             localStorage.setItem('username', data.username);
-            window.location.href = 'http://localhost:8000/studentHome';
             //localStorage.setItem('user_img', data.user_img);
+            localStorage.setItem('is_staff', data.is_staff);
+            if (data.is_staff === 'true'){
+                window.location.href = 'http://localhost:8000/staffHome';
+            }else{
+                window.location.href = 'http://localhost:8000/studentHome';
+            }
         }else{
             console.log('Error: Failed to obtain access-tokens', data);
             console.log('Access-token:', data.access_token);
